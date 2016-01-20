c4b75c778fdf6bc5c896d43043f0d77a42ffccf9
Kyle Kelley <rgbkrk@gmail.com>
2016-01-19 20:10:21 -0600

    Random walk and adjectives.

--- fakes.js
+++ fakes.js
@@ -1,10 +1,19 @@
 const codsworthNames = require('clean-codsworth-names')
 const mapSize = 2048
 
-// newPlayer creates a new player with a name and some random coordinates
+const uuid = require('uuid')
+const adjectives = require('adjectives')
+
+// Upper casing these for their name
+const Adjectives = adjectives.map(adj => {
+  return adj.charAt(0).toUpperCase() + adj.slice(1)
+})
+
 function newPlayer(name) {
+  const adj = Adjectives[Math.floor(Math.random() * Adjectives.length)];
+
   return {
-    name: name,
+    name: `${adj} ${name}`,
     x: Math.round(Math.random() * (mapSize - 1)),
     y: Math.round(Math.random() * (mapSize - 1)),
   }
@@ -13,5 +22,19 @@ function newPlayer(name) {
 // create a set of players with coordinates
 const players = codsworthNames.map(newPlayer)
 
-// Just to try this out, print out a few of these
-console.log(players.slice(0,4))
+// Random walk -1, 0, 1
+function walk(pt) {
+  const change = Math.cos(Math.PI * Math.round(Math.random())) // -1 or 1
+                 * Math.round(Math.random()) // 0 or 1
+  const newPt = pt + change
+  if (newPt < 0 || newPt >= mapSize) {
+    return pt
+  }
+  return newPt
+}
+
+const player = players[0]
+console.log(player)
+player.x = walk(player.x)
+player.y = walk(player.y)
+console.log(player)
